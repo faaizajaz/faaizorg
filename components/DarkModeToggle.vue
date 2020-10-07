93c34bce8989924f4198e1b673fc8e359d7d1e22
faaiz <faaizajaz@gmail.com>
2020-10-07 19:22:19 +0500

    Cleaned up inline SVGs (removed unnnecessary metadata)

--- components/DarkModeToggle.vue
+++ components/DarkModeToggle.vue
@@ -2,58 +2,14 @@
   <div id="dark-toggle-div">
     <svg
       id="light-mode-svg"
-      xmlns:dc="http://purl.org/dc/elements/1.1/"
-      xmlns:cc="http://creativecommons.org/ns#"
-      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      xmlns:svg="http://www.w3.org/2000/svg"
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
       width="24"
       height="24"
       viewBox="0 0 48 48"
       fill="none"
-      sodipodi:docname="sun-light.svg"
-      inkscape:version="1.0.1 (1.0.1+r73)"
     >
-      <metadata
-        id="metadata44"
-      >
-        <rdf:RDF>
-          <cc:Work
-            rdf:about=""
-          >
-            <dc:format>image/svg+xml</dc:format>
-            <dc:type
-              rdf:resource="http://purl.org/dc/dcmitype/StillImage"
-            />
-            <dc:title />
-          </cc:Work>
-        </rdf:RDF>
-      </metadata>
       <defs
         id="defs42"
-      />
-      <sodipodi:namedview
-        id="namedview40"
-        pagecolor="#ffffff"
-        bordercolor="#666666"
-        borderopacity="1"
-        objecttolerance="10"
-        gridtolerance="10"
-        guidetolerance="10"
-        inkscape:pageopacity="0"
-        inkscape:pageshadow="2"
-        inkscape:window-width="1920"
-        inkscape:window-height="1022"
-        showgrid="false"
-        inkscape:zoom="35.916667"
-        inkscape:cx="12.055684"
-        inkscape:cy="9.1856167"
-        inkscape:window-x="1920"
-        inkscape:window-y="0"
-        inkscape:window-maximized="1"
-        inkscape:current-layer="svg38"
       />
       <path
         id="path4"
@@ -209,58 +165,14 @@
     <span>&nbsp;</span>
     <svg
       id="dark-mode-svg"
-      xmlns:dc="http://purl.org/dc/elements/1.1/"
-      xmlns:cc="http://creativecommons.org/ns#"
-      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-      xmlns:svg="http://www.w3.org/2000/svg"
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
       width="24"
       height="24"
       viewBox="0 0 48 48"
       fill="none"
-      version="1.1"
-      sodipodi:docname="moon-light.svg"
-      inkscape:version="1.0.1 (1.0.1+r73)"
     >
-      <metadata
-        id="metadata12"
-      >
-        <rdf:RDF>
-          <cc:Work
-            rdf:about=""
-          >
-            <dc:format>image/svg+xml</dc:format>
-            <dc:type
-              rdf:resource="http://purl.org/dc/dcmitype/StillImage"
-            />
-          </cc:Work>
-        </rdf:RDF>
-      </metadata>
       <defs
         id="defs10"
-      />
-      <sodipodi:namedview
-        id="namedview8"
-        pagecolor="#ffffff"
-        bordercolor="#666666"
-        borderopacity="1"
-        objecttolerance="10"
-        gridtolerance="10"
-        guidetolerance="10"
-        inkscape:pageopacity="0"
-        inkscape:pageshadow="2"
-        inkscape:window-width="1920"
-        inkscape:window-height="1022"
-        showgrid="false"
-        inkscape:zoom="35.916667"
-        inkscape:cx="12.055684"
-        inkscape:cy="12"
-        inkscape:window-x="1920"
-        inkscape:window-y="0"
-        inkscape:window-maximized="1"
-        inkscape:current-layer="svg6"
       />
       <rect
         id="rect2"
